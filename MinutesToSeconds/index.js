b8a75f3cae6cf0fc0b93e1a741dd27cb281d57dd
NtandoNokilana <ntandonokilana@gmail.com>
2023-11-23 14:18:50 +0200

    updated file

--- MinutesToSeconds/index.js
+++ MinutesToSeconds/index.js
@@ -1,12 +1,10 @@
 function convertMinstoSecs() {
     // Get the value entered in the 'minutes' input
-    var minutes = document.getElementById('minutes').value;
-
+    let minutes = document.getElementById('minutes').value;
     // Validate if the input is a valid number
     if (!isNaN(minutes) && minutes !== "") {
         // Convert minutes to seconds
-        var seconds = minutes * 60;
-
+        let seconds = minutes * 60;
         // Update the 'seconds' input with the result
         document.getElementById('seconds').value = seconds;
     } else {
